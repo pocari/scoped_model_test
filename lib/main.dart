30d4056739935f1142c87c266a8027190c45b5d0
pocari <caffelattenonsugar@gmail.com>
2019-11-11 21:34:03 +0900

    scoped modeでつくってみる

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+    title: 'Scoped Model Test',
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Scoped Model Test'),
+      ),
+      body: CounterApp(),
+    )));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
+class CounterModel extends Model {
+  int _counter = 0;
+
+  int get counter => _counter;
+
+  void increment() {
+    _counter++;
+
+    notifyListeners();
+  }
+}
+
+class CounterApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    return null;
+    return ScopedModel<CounterModel>(
+      model: CounterModel(),
+      child: Column(
+        children: <Widget>[
+          ScopedModelDescendant<CounterModel>(
+            builder: (ctx, child, model) => Text('${model.counter}'),
+          ),
+          ScopedModelDescendant<CounterModel>(
+            builder: (ctx, child, model) => RaisedButton(
+              onPressed: model.increment,
+              child: Text('Plus Button'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
